fd88cc5547de32f83bc20767257a0b2d78a41b5a
Tee <saltandtee@gmail.com>
2021-12-22 08:38:31 +0100

    oops

--- server/start/routes.ts
+++ server/start/routes.ts
@@ -18,8 +18,26 @@
 |
 */
 
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-})
+Route.get('/', async () => ({
+  uptime: process.uptime(),
+}))
+
+
+Route.get('/health', async () => ({ report: await HealthCheck.getReport() }))
+
+Route.post('/summoner/basic', 'SummonersController.basic')
+Route.post('/summoner/overview', 'SummonersController.overview')
+
+Route.post('/summoner/champions', 'SummonersController.champions')
+Route.post('/summoner/records', 'SummonersController.records')
+Route.post('/summoner/live', 'SummonersController.liveMatchDetails')
+
+Route.post('/match', 'MatchesController.index')
+Route.post('/match/details', 'MatchesController.show')
+Route.post('/match/details/ranks', 'MatchesController.showRanks')
+
+Route.get('/cdragon/runes', 'CDragonController.runes')
+
